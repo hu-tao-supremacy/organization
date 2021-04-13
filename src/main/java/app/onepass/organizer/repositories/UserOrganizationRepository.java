159c2465ebd0c2f383e7902515e88f4264b0ba4f
ChanyutJean <chanyut22@hotmail.com>
2021-04-13 15:16:21 +0700

    add getUser rpc call

--- src/main/java/app/onepass/organizer/repositories/UserOrganizationRepository.java
+++ src/main/java/app/onepass/organizer/repositories/UserOrganizationRepository.java
@@ -1,10 +1,14 @@
 package app.onepass.organizer.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.onepass.organizer.entities.UserOrganizationEntity;
 
 public interface UserOrganizationRepository extends JpaRepository<UserOrganizationEntity, Integer> {
+
+	List<UserOrganizationEntity> findByOrganizationId(int organizationId);
 
 	UserOrganizationEntity findByUserIdAndOrganizationId(int userId, int organizationId);
 }
