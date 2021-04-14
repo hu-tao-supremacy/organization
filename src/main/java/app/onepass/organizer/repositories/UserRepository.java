a9b129695b16d0a8c7f3604c86d86ada38d0512d
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-14 22:43:53 +0700

    feat(repositories): add missing import statements

--- src/main/java/app/onepass/organizer/repositories/UserRepository.java
+++ src/main/java/app/onepass/organizer/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package app.onepass.organizer.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import app.onepass.organizer.entities.UserEntity;
 
 @Repository
