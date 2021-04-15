696fcd97281cba9dfc61d1c33cf8ca41ae69c1e6
ChanyutJean <chanyut22@hotmail.com>
2021-04-15 16:59:45 +0700

    code location business logic

--- src/main/java/app/onepass/organizer/repositories/LocationRepository.java
+++ src/main/java/app/onepass/organizer/repositories/LocationRepository.java
@@ -1,5 +1,7 @@
 package app.onepass.organizer.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import app.onepass.organizer.entities.LocationEntity;
 
 @Repository
 public interface LocationRepository extends JpaRepository<LocationEntity, Integer> {
+
+	List<LocationEntity> findByName(String name);
 }
